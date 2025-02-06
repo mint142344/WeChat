0d18b12a4c020585102ec8ab5d483a3cbabef3a8
hzh <1423443710@qq.com>
2025-02-06 11:40:29 +0800

    初始化仓库

---
+++ LoginDialog.cpp
@@ -0,0 +1,8 @@
+#include "LoginDialog.h"
+#include "ui_LoginDialog.h"
+
+LoginDialog::LoginDialog(QWidget *parent) : QDialog(parent), ui(new Ui::LoginDialog) {
+	ui->setupUi(this);
+}
+
+LoginDialog::~LoginDialog() { delete ui; }
